2d9758595ff2ba3a409a960aa89238c39c9e1041
conquerswap <conquerswap@gmail.com>
2021-04-07 15:52:07 +0100

    update

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xA9AD102640fbcf9E177c647335130be39652006e',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: ''
+  lpAddress: '0xbdeff7ef2e5dfa51bcc728df36cdd8e683fcc323'
 }
 
 export default priceContracts
